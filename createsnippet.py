bac8dcb57c0be51be01b0d69d879a17be3ca1850
Carson Shields <carson3822@icloud.com>
2023-06-01 22:42:09 -0400

    Added basic function to snip wav audio file at specified range in sec.

--- createsnippet.py
+++ createsnippet.py
@@ -1,14 +1,19 @@
 import json
 from pydub import AudioSegment
 from datetime import datetime
-from info import nm
+from info import nm, filepath
 
 today = datetime.today().strftime('%m-%d')
 name = nm
-t1 = 0
-t2 = 176
-b = t1 * 1000
-e = t2 * 1000
-output_audio = AudioSegment.from_file("savedaudiofiles/Meeting1:05-29.wav")
-output_audio = output_audio[b:e]
-output_audio.export(f"./savedaudiofiles/{name}-missed-meeting:{today}-.wav", format="wav", bitrate="1411k", codec="pcm_s16le")
+
+
+def take_snippet(name: str, begin: int, end: int):
+    """Takes wav audio file and outputs a snipped version at begin(sec) and end(sec)"""
+    b = begin * 1000
+    e = end * 1000
+    output_audio = AudioSegment.from_file(filepath)
+    output_audio = output_audio[b:e]
+    output_audio.export(f"./savedaudiofiles/{name}-missed-meeting:{today}-.wav", format="wav", bitrate="1411k", codec="pcm_s16le")
+
+
+take_snippet(nm, 0, 176)
