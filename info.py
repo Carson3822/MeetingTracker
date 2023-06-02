84a7b7ec73450cf6b662c0cd873b292a566c153f
Carson Shields <97532220+Carson3822@users.noreply.github.com>
2023-06-01 22:15:18 -0400

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,1 @@
-s_a = 'practiceemail87@gmail.com'
-s_p = "xrnxdfcpllpodgkh"
 
-rec_a = 'carson3822@icloud.com'
-YOUR_SECRET_KEY = "fbf3f8384d6d3406abe538c758c59f55d6032ca1"
-
-filepath = 'savedaudiofiles/Meeting1: 05-29.WAV'
