62e843f6c8838b168651a5fef09e0e9cd68042b5
Carson Shields <carson3822@icloud.com>
2023-06-04 10:48:02 -0400

    Updated comments to better reflect updated functions

--- JSONparser.py
+++ JSONparser.py
@@ -8,7 +8,8 @@ with open(data_path, "r") as read_file:
     data = json.load(read_file)
 
 
-# returns a list of names of members who are not present ("attendance" key set to False)
+# returns a dictionary with names of non-present members as keys and empty lists as the value
+# {missing members name: []} (if "attendance" key set to False)
 def find_absent_members(team_mems: dict):
     absent_li = {}
     for i in team_mems:
@@ -18,8 +19,8 @@ def find_absent_members(team_mems: dict):
     return absent_li
 
 
-# searches for names of missing members in API response data and returns a dictionary with keys = missing members names
-# and values = to a list of floats that represent each time that name was mentioned in input audio
+# searches for names of missing members in API response data and returns the output dict from find_absent_members
+# {missing members name: [time_stamps]}
 def find_names(dict_names: dict):
     """Returns a list of lists from json output with [name: time_stamp] in seconds with a <float> at list[-1] representing the end of the input wav file in seconds"""
     wordz = data["results"]["channels"][0]["alternatives"][0]["words"]
