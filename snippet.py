68ab8b0df72e779d67be3e953a57a98c14a53bd7
Carson Shields <carson3822@icloud.com>
2023-06-04 14:11:59 -0400

    cleared out print statements

--- snippet.py
+++ snippet.py
@@ -19,16 +19,14 @@ def save_snippet(name: str, begin: float, end: float, n: int):
     """Takes wav audio file and outputs a snipped version at begin(sec) and end(sec)"""
     output_audio = AudioSegment.from_file(input_audio_filepath)
     output_audio = output_audio[begin:end]
-    out_file_path = f"./OutputData/snippedwav/{name}--missed-meeting-{today}--{n}.wav"
+    out_file_path = f"./OutputData/snippedaudio/{name}--missed-meeting-{today}--{n}.wav"
     output_audio.export(out_file_path, format="wav", bitrate="1411k", codec="pcm_s16le")
 
 
 def set_timestamps(absnt_info):
     """dynamically sets length of audio snippet then saves data"""
-    print(absnt_info.keys())
     for i in absnt_info.keys():
         if i != "endclip":
-            print(i)
             beg_ts = (absnt_info[i][0] - 120 if absnt_info[i][0] - 120 > 0 else 0) * 1000
             end_ts = 0
             n = 0
@@ -37,14 +35,11 @@ def set_timestamps(absnt_info):
                 if prev_ts == 0:
                     end_ts = (curr_time + 120 if curr_time + 120 < absnt_info["endclip"] else absnt_info["endclip"])
                     prev_ts = curr_time
-                    print(prev_ts, end_ts, "1")
                 elif curr_time < prev_ts + 120 < absnt_info["endclip"]:
                     end_ts = (end_ts + 10)
                     prev_ts = curr_time
-                    print(prev_ts, end_ts, "2")
-            print(end_ts)
             n += 1
-            save_snippet(i, beg_ts * 1000, end_ts * 1000, n)
+            save_snippet(i.capitalize(), beg_ts * 1000, end_ts * 1000, n)
 
 
 if __name__ == "__main__":
